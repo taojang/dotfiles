19848e958e7e656fd901d5a90eae170b9134502a
Tao Yang <tao.yang@zalando.de>
2015-10-30 10:58:10 +0100

    auto install ensime

--- emacs-prelude-personal/init-scala.el
+++ emacs-prelude-personal/init-scala.el
@@ -1,6 +1,7 @@
 ;;(prelude-require-package 'ensime)
 
+(prelude-require-package 'ensime)
 (add-to-list 'load-path "/usr/share/ensime/elisp")
 (add-to-list 'exec-path "/usr/share/ensime")
-(require 'ensime)
+
 (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
